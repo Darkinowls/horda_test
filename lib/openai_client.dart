65d6fd2ff2bca58862ccf38a5d0ab67482029b27
Denys Chernousov <blackInOwls@gmail.com>
2023-11-04 14:12:09 +0200

    dio timeout and throttling

--- lib/openai_client.dart
+++ lib/openai_client.dart
@@ -6,15 +6,19 @@ class OpenaiClient {
 
   OpenaiClient(this._dio, this._token) {
     _dio.options.baseUrl = "https://api.openai.com/v1";
-    _dio.options.connectTimeout = const Duration(milliseconds: 5000);
+    _dio.options.connectTimeout = const Duration(seconds: 6);
+    _dio.options.receiveTimeout = const Duration(seconds: 6);
+    _dio.options.sendTimeout = const Duration(seconds: 6);
     _dio.options.headers = {
       "Authorization": "Bearer $_token",
       "Content-Type": "application/json"
     };
+
   }
 
   Future<String> generateImageUrl(String prompt) async {
-    final res = await _dio.post("/images/generations",
+    await Future.delayed(const Duration(milliseconds: 50));
+    final Response res = await _dio.post("/images/generations",
         data: {"prompt": prompt, "n": 1, "size": "256x256"});
 
     return res.data["data"][0]["url"];
